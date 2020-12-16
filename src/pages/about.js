9a3dd46bd19a4c4dd392999c292f5bb14b4943df
Ronald <wraith55@gmail.com>
2020-12-15 22:41:26 -0500

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <React.Fragment>
+    <div>About page</div>
+    <Link to="/">Back to home</Link>
+  </React.Fragment>
+)
